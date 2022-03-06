91ee23b636fa2fba9ed5448520eddffeb19811e5
Bob D'Errico <bobderrico80@gmail.com>
2022-03-06 16:07:58 -0500

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     title: `Bob D'Errico - Software Engineer`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+  ],
 };
